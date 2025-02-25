407b2a705c9eb303a8172003add4d8a3711494d0
Gustav-hdoliveira <162641711+Gustav-hdoliveira@users.noreply.github.com>
2025-02-24 21:47:01 -0300

    Add files via upload

--- app/src/main/java/conexaoBanco/InserirUsuario.java
+++ app/src/main/java/conexaoBanco/InserirUsuario.java
@@ -4,10 +4,24 @@
  */
 package conexaoBanco;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author GUSTAVOHENRIQUEDEOLI
  */
 public class InserirUsuario {
-    
+    public static void inserirUsuario(Connection conexao, String nome, String email) throws SQLException{
+        String sql = "INSERT INTO usuarios (nome, email) VALUES (?, ?)";
+        
+        try(PreparedStatement pstmt = conexao.prepareStatement(sql)){
+            pstmt.setString(1, nome); // Substitui o primeiro ? por 'nome'
+            pstmt.setString(2, email);
+            pstmt.executeUpdate();
+            
+            System.out.println("Usuário inserido com sucesso");
+        }
+    }
 }
